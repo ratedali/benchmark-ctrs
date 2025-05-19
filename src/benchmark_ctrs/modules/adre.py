f3cb3ef069b659cf08049d260b7d238a9fb9e11f
Ali Salaheldin Ali Ahmed <alsa00006@stud.uni-saarland.de>
2025-05-19 21:27:30 +0200

    add ADRE

---
+++ src/benchmark_ctrs/modules/adre.py
@@ -0,0 +1,137 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, cast
+
+import torch
+import torch.nn.functional as F
+import torchmetrics
+import torchmetrics.aggregation
+from torch.profiler import record_function
+from typing_extensions import Literal, override
+
+from benchmark_ctrs.modules.rs_training import (
+    Batch,
+    HParams,
+    RSTrainingModule,
+    StepOutput,
+)
+
+if TYPE_CHECKING:
+    from torch import Tensor
+
+    from benchmark_ctrs.modules.rs_training import (
+        Batch,
+        StepOutput,
+    )
+
+
+@dataclass(frozen=True)
+class ADREHParams(HParams):
+    learning_rate: float = 0.1
+    lr_decay: float = 0.1
+    lr_step: int = 50
+
+    k: int = 8
+    lbd: float = 0.1
+    adversarial: bool = False
+
+
+class ADRE(RSTrainingModule):
+    def __init__(
+        self,
+        *args,
+        params: ADREHParams,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs, params=params)
+        self._Lper = {
+            "train": torchmetrics.aggregation.MeanMetric(),
+            "val": torchmetrics.aggregation.MeanMetric(),
+        }
+        self._Ladre = {
+            "train": torchmetrics.aggregation.MeanMetric(),
+            "val": torchmetrics.aggregation.MeanMetric(),
+        }
+
+    @override
+    def training_step(
+        self,
+        batch: tuple[Tensor, ...],
+        batch_idx: int,
+        dataloader_idx: int | None = None,
+    ) -> StepOutput:
+        skip_rl = (
+            self.hparams["deferred"] and self.current_epoch < self.hparams["lr_step"]
+        )
+        return self._loss(batch, skip_rl=skip_rl, prefix="train")
+
+    @override
+    def validation_step(
+        self,
+        batch: tuple[Tensor, ...],
+        batch_idx: int,
+        dataloader_idx: int | None = None,
+    ) -> StepOutput:
+        return self._loss(batch, prefix="val")
+
+    def _loss(
+        self,
+        batch: Batch,
+        *,
+        skip_rl: bool = False,
+        prefix: Literal["train", "val"] | None = None,
+    ) -> StepOutput:
+        hparams = cast("ADREHParams", self.hparams)
+        inputs, targets = batch
+
+        with record_function("sampling"):
+            samples = torch.repeat_interleave(inputs, hparams.k, dim=0)  # (B*k)xCxWxH
+            sample_logits: torch.Tensor = self(samples)  # (B*k)xK
+            sample_targets = torch.repeat_interleave(targets, hparams.k)
+
+        with record_function("classification_loss"):
+            R_per: torch.Tensor = self._criterion(
+                sample_logits,
+                sample_targets,
+            )
+
+        if hparams.lbd == 0:
+            # input_probs will only need gradients if robust loss is also needed
+            sample_logits = sample_logits.detach()
+
+        with record_function("robust_loss"):
+            # smoothed class probabilities \hat{G}(x), shape same as targets
+            input_probs = (
+                torch.softmax(sample_logits, dim=1)
+                .unflatten(dim=0, sizes=(-1, hparams.k))
+                .mean(dim=1)
+            )
+
+            R_adre = torch.tensor(0)
+            if hparams.lbd != 0:
+                class_logprobs = torch.log(input_probs + 1e-10)  # avoid NaN results
+                top2 = torch.topk(class_logprobs, 2)
+                use_c2 = (
+                    top2.indices[:, 0] == targets
+                )  # argmax_{c} \hat{G}^c(x) ==  y_i, thus use second top class
+
+                R_adre = -F.nll_loss(
+                    class_logprobs[use_c2],
+                    top2.indices[use_c2, 1],
+                    reduction="sum",
+                )
+                R_adre += -F.nll_loss(
+                    class_logprobs[~use_c2],
+                    top2.indices[~use_c2, 0],
+                    reduction="sum",
+                )
+                R_adre = R_adre / targets.numel()
+
+        if prefix is not None:
+            self._Lper[prefix](R_per)
+            self._Ladre[prefix](R_adre)
+            self.log("{prefix}/classification_loss", self._Lper[prefix], on_epoch=True)
+            self.log("{prefix}/robust_loss", self._Ladre[prefix], on_epoch=True)
+
+        return {"loss": R_per + hparams.lbd * R_adre, "predictions": input_probs}
