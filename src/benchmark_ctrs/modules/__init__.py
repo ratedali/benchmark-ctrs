edb121b7d0959de86f8346fc7ddec09597dedcac
Ali Salaheldin Ali Ahmed <alsa00006@stud.uni-saarland.de>
2025-05-21 12:36:17 +0200

    add ADRE in CLI

--- src/benchmark_ctrs/modules/__init__.py
+++ src/benchmark_ctrs/modules/__init__.py
@@ -1,8 +1,10 @@
+from benchmark_ctrs.modules.adre import ADRE
 from benchmark_ctrs.modules.gaussian_aug import GaussianAug
 from benchmark_ctrs.modules.macer import MACER
 from benchmark_ctrs.modules.rs_training import RandomizedSmoothing
 
 __all__ = [
+    "ADRE",
     "MACER",
     "GaussianAug",
     "RandomizedSmoothing",
