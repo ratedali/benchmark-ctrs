36e04433da41b98fbc585af621df71048244e70d
Ali Salaheldin Ali Ahmed <alsa00006@stud.uni-saarland.de>
2025-05-22 21:03:29 +0200

    improve CR metric typing and use inference mode

--- src/benchmark_ctrs/metrics/certified_radius.py
+++ src/benchmark_ctrs/metrics/certified_radius.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import dataclasses
-from typing import TYPE_CHECKING, Any, ClassVar
+from typing import TYPE_CHECKING, Any, ClassVar, NamedTuple
 
+import numpy as np
 import torch
+from lightning.pytorch.utilities import LightningEnum
 from torchmetrics import Metric
 from torchmetrics.utilities import dim_zero_cat
 from torchmetrics.utilities.exceptions import TorchMetricsUserError
@@ -11,9 +13,17 @@ from torchmetrics.utilities.exceptions import TorchMetricsUserError
 from benchmark_ctrs.models import smooth
 
 if TYPE_CHECKING:
+    from typing import Literal
+
     from torch import Tensor
     from torch.nn import Module
-    from typing_extensions import Literal
+
+
+class _Reduction(LightningEnum):
+    Mean = "mean"
+    Max = "max"
+    Min = "min"
+    None_ = "none"
 
 
 @dataclasses.dataclass(frozen=True)
@@ -21,6 +31,12 @@ class Params(smooth.HParams):
     start: int = 0
     skip: int = 1
     max_: int | None = None
+
+
+class CertificationResult(NamedTuple):
+    indices: Tensor
+    predictions: Tensor
+    radii: Tensor
 
 
 class CertifiedRadius(Metric):
@@ -46,8 +62,11 @@ class CertifiedRadius(Metric):
     ) -> None:
         super().__init__(**kwargs)
 
-        if reduction not in {"mean", "min", "max", "none"}:
-            raise TorchMetricsUserError(f"Unknown reduction mode: '{reduction}'")
+        if reduction not in list(_Reduction):
+            raise TorchMetricsUserError(
+                f"Unknown reduction mode '{reduction}'. "
+                f"Supported values: {[x.value for x in _Reduction]}"
+            )
 
         self._reduction = reduction
         self._start = params.start
@@ -64,38 +83,51 @@ class CertifiedRadius(Metric):
             self.add_state("_predictions", default=[], dist_reduce_fx="cat")
             self.add_state("_indices", default=[], dist_reduce_fx="cat")
 
-    def update(self, inputs: Tensor):
+    @torch.inference_mode()
+    def update(self, inputs: Tensor) -> None:
         indices = torch.arange(
             start=self._start,
             end=self._max if self._max is not None else inputs.size(0),
             step=self._skip,
-        )
-        certs = [self._smooth.forward(x, certify=True) for x in inputs[indices]]
-        radii = torch.tensor(
-            [cert.certified_radius for cert in certs],
-            dtype=torch.float,
             device=self.device,
+            dtype=torch.int,
         )
-        self._radii.append(torch.as_tensor(radii, device=self.device))
 
+        certs = [self._smooth.forward(x, certify=True) for x in inputs[indices]]
+        radii = np.fromiter(
+            iter=(cert.certified_radius for cert in certs),
+            dtype=float,
+        )
+
+        self._radii.append(torch.from_numpy(radii).to(self.device))
         if self._reduction == "none":
-            predictions = torch.tensor(
-                [
-                    cert.prediction if isinstance(cert.prediction, int) else -1
-                    for cert in certs
-                ],
-                dtype=torch.int,
-                device=self.device,
-            )
             self._indices.append(indices)
-            self._predictions.append(predictions)
+            predictions = np.fromiter(
+                iter=(
+                    cert.prediction if not smooth.is_abstain(cert.prediction) else -1
+                    for cert in certs
+                ),
+                dtype=int,
+            )
+            self._predictions.append(torch.from_numpy(predictions).to(self.device))
 
-    def compute(self) -> Tensor | tuple[Tensor, Tensor, Tensor]:
+    @torch.inference_mode()
+    def compute(self) -> Tensor | CertificationResult:
+        """Compute the certified radius metric value
+
+        Returns:
+            Tensor | CertificationResult: returns the CertificationResults tuple
+            if reduction="none", otherwise it returns the aggregated certified radii
+        """
         radii = dim_zero_cat(self._radii)
         if self._reduction == "none":
             indices = dim_zero_cat(self._indices)
             predictions = dim_zero_cat(self._predictions)
-            return indices, predictions, radii
+            return CertificationResult(
+                indices=indices,
+                predictions=predictions,
+                radii=radii,
+            )
         if radii.numel() == 0:
             return torch.tensor([0.0]).to(radii)
         if self._reduction == "max":
