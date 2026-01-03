ee996b58029e58523a0868d66aad70c893ca3a01
Ali Salaheldin Ali Ahmed <alsa00006@stud.uni-saarland.de>
2026-01-03 19:27:39 +0100

    simplfy GaussianAug

--- src/benchmark_ctrs/modules/gaussian_aug.py
+++ src/benchmark_ctrs/modules/gaussian_aug.py
@@ -1,7 +1,6 @@
 from typing import Any
 
 import torch
-from torch import Tensor
 from torch.profiler import record_function
 from typing_extensions import override
 
@@ -10,20 +9,6 @@ from benchmark_ctrs.types import Batch, StepOutput
 
 
 class GaussianAug(BaseModule):
-    @override
-    def forward(
-        self,
-        inputs: Tensor,
-        *args: Any,
-        add_noise: bool = False,
-        **kwargs: Any,
-    ) -> Tensor:
-        sigma = self.hparams["sigma"]
-        if add_noise and sigma != 0:
-            noises = torch.randn_like(inputs) * sigma
-            inputs = torch.clamp(inputs + noises, 0.0, 1.0)
-        return super().forward(inputs, *args, **kwargs)
-
     @override
     def training_step(self, batch: Batch, *args: Any, **kwargs: Any) -> StepOutput:
         return self.gaussian_aug_step(batch)
@@ -35,7 +20,10 @@ class GaussianAug(BaseModule):
     def gaussian_aug_step(self, batch: Batch) -> StepOutput:
         inputs, targets = batch[:2]
         with record_function("sampling"):
-            predictions = self.forward(inputs, add_noise=True)
+            sigma = self.hparams["sigma"]
+            noisy_inputs = inputs + torch.randn_like(inputs) * sigma
+            noisy_inputs = torch.clamp(noisy_inputs, 0.0, 1.0)
+            predictions = self.forward(noisy_inputs)
         with record_function("classification_loss"):
             loss = self.criterion(predictions, targets)
         return {"loss": loss, "predictions": predictions}
